73b932ccdf2e96f35274948359ed789ef3aa4e07
Ujwala <ujwalap801@gmail.com>
2025-07-05 21:48:22 +0530

    Added Closures Folder

---
+++ Closures/Hello_World.js
@@ -0,0 +1,15 @@
+// @return {Function}
+ 
+var createHelloWorld = function() {
+    
+    return function(...args) {
+       return "Hello World";
+    }
+};
+
+const f =createHelloWorld();
+console.log(f());
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
